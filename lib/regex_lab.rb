11bab814f3c33dc0892f646e918092b5ce6014ab
Elaine Anderson <eparie@bu.edu>
2018-05-17 23:08:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
- text.scan(/[A-Z]\w*[!\.?]\z/).any?
+ if text.scan(/[A-Z]\w*[!\.?]\z/).any?
   end
+end
 
 def valid_phone_number?(phone)
 
