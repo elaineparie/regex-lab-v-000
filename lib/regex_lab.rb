102a0000b7f114acaa64cceb4d0fea14edccf939
Elaine Anderson <eparie@bu.edu>
2018-05-17 23:00:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,18 +1,19 @@
 def starts_with_a_vowel?(word)
+ word.scan(/\A[aeiouAEIOU]/).any?
+ end
 
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
+ text.scan(/\b[A-Z]\w*[!\.?]/).any?
+  end
 
 def valid_phone_number?(phone)
 
